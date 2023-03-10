02af0fadd637492132809bd98c2cf827577bb987
jamesjavanyc` <1931736857@qq.com>
2023-03-10 00:43:53 -0500

    123

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const { ApolloServer } = require("apollo-server-express");
-export {}
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
